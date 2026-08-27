3d3c056a14e907b115e41fef0c1cc2a319e1b869
Julien Schueller <schueller@phimeca.com>
2026-08-27 18:14:31 +0200

    Enable CMAKE_UNITY_BUILD

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,11 +8,11 @@ cmake ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_UNITY_BUILD=ON ^
     -DBUILD_BENCHMARKS=no ^
     -DINTEGER_CLASS=flint ^
     -DWITH_MPC=yes ^
     -DWITH_LLVM=yes ^
-    -DWITH_COTIRE=no ^
     -DWITH_SYMENGINE_THREAD_SAFE=yes ^
     -DBUILD_FOR_DISTRIBUTION=yes ^
     -DBUILD_SHARED_LIBS=yes ^
@@ -21,15 +21,8 @@ cmake ^
     ..
 if errorlevel 1 exit 1
 
-ninja -j%CPU_COUNT%
+ninja install -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
-ninja install
-if errorlevel 1 exit 1
-
-if "%PKG_VERSION%"=="0.12.0" (
-   ctest -E test_cwrapper --output-on-failure
-) else (
-   ctest --output-on-failure
-)
+ctest --output-on-failure
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,12 @@ if [[ "${target_platform}" == "linux-ppc64le" ]]; then
   export CXXFLAGS="${CXXFLAGS//-fno-plt/}"
 fi
 
-mkdir build
-cd build
-
 cmake ${CMAKE_ARGS} \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_UNITY_BUILD=ON \
     -DBUILD_BENCHMARKS=no \
     -DINTEGER_CLASS=flint \
     -DWITH_SYMENGINE_THREAD_SAFE=yes \
@@ -22,11 +20,10 @@ cmake ${CMAKE_ARGS} \
     -DBUILD_FOR_DISTRIBUTION=yes \
     -DBUILD_SHARED_LIBS=yes \
     -DWITH_LLVM_DYLIB=no \
-    ..
+    -B build .
 
-cmake --build . -- -j${CPU_COUNT}
-cmake --build . --target install
+cmake --build build --target install -j${CPU_COUNT}
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-ctest
+ctest --test-dir build --output-on-failure
 fi
